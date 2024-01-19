4ff88daf4aa7e4c789e09eedb243adb75bb2fb57
박희준 <wooryjoon@gmail.com>
2024-01-19 16:14:07 +0900

    Feat : Header Dialog UI 구현, Interaction 추가

--- frontend/src/components/Header/Logo.tsx
+++ frontend/src/components/Header/Logo.tsx
@@ -1,5 +1,5 @@
 type Props = {};
 
 export default function Logo({}: Props) {
-    return <div className="logo-container">AI로야구하이라이트생성하는사이트임</div>;
+    return <div className="logo-container">AI로야구하이라이트생성하는사이트</div>;
 }
