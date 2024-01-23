74a3335e2ff96800f8a594a2247293c21b37fbf4
박희준 <wooryjoon@gmail.com>
2024-01-23 12:04:07 +0900

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import '@/styles/App.scss';
+import Header from '@/components/Header';
+function App() {
+    return (
+        <>
+            <section id="section">
+                <Header></Header>
+            </section>
+        </>
+    );
+}
+
+export default App;
