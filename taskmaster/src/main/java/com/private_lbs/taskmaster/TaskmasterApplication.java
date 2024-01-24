16ed2a5445905535746435c2afd88efc882f5e10
BoyCho <iijokv@naver.com>
2024-01-24 09:01:26 +0900

    init

---
+++ taskmaster/src/main/java/com/private_lbs/taskmaster/TaskmasterApplication.java
@@ -0,0 +1,13 @@
+package com.private_lbs.taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterApplication.class, args);
+	}
+
+}
