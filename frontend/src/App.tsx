f56813505f8b16c337f8d2ca08822e6917fd3ddd
minjeong <minj_1203@naver.com>
2024-01-25 16:59:06 +0900

    Feature : LandingPage 라우팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import LandingPage from './pages/LandingPage';
 import Video from './pages/Video';
-import InputPlayer from './pages/Video/InputPlayer';
 import UploadVideo from './pages/Video/UploadVideo';
+import Login from './pages/Login';
 
 function App() {
     let vh = window.innerHeight * 0.01;
@@ -16,10 +16,9 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<LandingPage />} />
-                        {/* <Route path="/login" element={<LogIn/>} /> */}
+                        <Route path="/login" element={<Login/>} />
                         <Route path="/signup" element={<SignUp />} />
                         <Route path="/video" element={<Video />} />
-                        <Route path="/inputplayer" element={<InputPlayer />} />
                         <Route path="/uploadvideo" element={<UploadVideo />} />
                     </Routes>
                 </BrowserRouter>
