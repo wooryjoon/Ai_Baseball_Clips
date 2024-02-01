76379f0803ba8ebf0b1d9c9b1d3529448a092aa5
hamseungchan <head0618@naver.com>
2024-02-01 13:01:14 +0900

    refactor : email 대신 사용자 ID로 subject 설정

--- src/main/java/com/private_lbs/taskmaster/global/util/JWTUtil.java
+++ src/main/java/com/private_lbs/taskmaster/global/util/JWTUtil.java
@@ -1,8 +1,8 @@
 package com.private_lbs.taskmaster.global.util;
 
 import com.private_lbs.taskmaster.member.data.vo.JwtToken;
+import com.private_lbs.taskmaster.member.entity.Member;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,7 +28,7 @@ public class JWTUtil {
     private long refreshTokenExpireTime;
 
     //	Signature 설정에 들어갈 key 생성.
-    private byte[] generateKey() {
+    public byte[] generateKey() {
         byte[] key = null;
         try {
             // charset 설정 안하면 사용자 플랫폼의 기본 인코딩 설정으로 인코딩 됨
@@ -39,17 +39,16 @@ public class JWTUtil {
         return key;
     }
 
-    public JwtToken createToken(String email) {
-        return JwtToken.of(createAccessToken(email), createRefreshToken(email));
+    public JwtToken createToken(Member member) {
+        return JwtToken.of(createAccessToken(member.getId()), createRefreshToken(member.getId()));
     }
 
-    public String createAccessToken(String email) {
-        return create(email, "access-token", accessTokenExpireTime);
+    public String createAccessToken(Long id) {
+        return create(id, accessTokenExpireTime);
     }
 
-    public String createRefreshToken(String email) {
-        return create(email, "refresh-token"
-                , refreshTokenExpireTime);
+    public String createRefreshToken(Long id) {
+        return create(id, refreshTokenExpireTime);
     }
 
 
@@ -59,34 +58,17 @@ public class JWTUtil {
     //	subject : payload에 sub의 value로 들어갈 subject값
     //	expire : 토큰 유효기간 설정을 위한 값
     //	jwt 토큰의 구성 : header + payload + signature
-    private String create(String email, String subject, long expireTime) {
+    private String create(Long id, long expireTime) {
         Claims claims = Jwts.claims()
-                .setSubject(subject) // 토큰 제목 설정 ex) access-token, refresh-token
+                .setSubject(id.toString()) // 토큰 제목 설정 ex) access-token, refresh-token
                 .setIssuedAt(new Date()) // 생성일 설정
                 .setExpiration(new Date(System.currentTimeMillis() + expireTime)); // 만료일 설정 (유효 기간)
 
-        //  저장할 data의 key, value
-        claims.put("email", email);
-
         String jwt = Jwts.builder()
-                .setHeaderParam("typ", "JWT").setClaims(claims)
+                .setHeaderParam("type", "JWT").setClaims(claims)
                 .signWith(SignatureAlgorithm.HS256, this.generateKey())
                 .compact();
 
         return jwt;
-    }
-
-    //  전달 받은 토큰이 제대로 생성된것인지 확인
-    public boolean checkToken(String token) {
-        System.out.println(token);
-        try {
-//			setSigningKey : JWS 서명 검증을 위한  secret key 세팅
-//			parseClaimsJws : 파싱하여 원본 jws 만들기
-            Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            System.out.println("틀림");
-            return false;
-        }
     }
 }
