b46f82de6d176dee9678d7061ad25f56703d4bb0
BoyCho <iijokv@naver.com>
2024-02-01 15:33:55 +0900

    feat : BaseEntity 구현

--- src/main/java/com/private_lbs/taskmaster/member/entity/Member.java
+++ src/main/java/com/private_lbs/taskmaster/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.private_lbs.taskmaster.member.entity;
 
+import com.private_lbs.taskmaster.global.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +12,7 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue
