f9f697a4bb79e0f692136f1d51ad881f3d0b2c86
hamseungchan <head0618@naver.com>
2024-02-01 13:09:59 +0900

    feat : AuthenticationInterceptor 등록

---
+++ src/main/java/com/private_lbs/taskmaster/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.private_lbs.taskmaster.global.config;
+
+import com.private_lbs.taskmaster.global.auth.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor);
+    }
+}
