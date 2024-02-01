3696f425e3080bd5f99711fdd2382cd6d7cbd378
hamseungchan <head0618@naver.com>
2024-02-02 08:53:38 +0900

    feat : CORS 설정

--- src/main/java/com/private_lbs/taskmaster/global/config/WebConfig.java
+++ src/main/java/com/private_lbs/taskmaster/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.private_lbs.taskmaster.global.config;
 import com.private_lbs.taskmaster.global.auth.AuthenticationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")  // CORS 적용할 URL 패턴
+                .allowedHeaders("Authorization", "Content-Type")    // CORS 요청에 허용되는 헤더
+                .allowedOrigins("*")    // 자원 공유를 허락할 Origin 지정
+                .allowedMethods("*")    // 허용할 HTTP method
+                .allowCredentials(true) // Credentials 포함 여부
+                .maxAge(3600);  // Pre-flight 리퀘스트 캐싱
     }
 }
