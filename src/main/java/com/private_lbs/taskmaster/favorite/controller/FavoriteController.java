657c89e5c29e552f56a49fe4dbd5399337d253f5
hamseungchan <head0618@naver.com>
2024-02-05 16:31:26 +0900

    feat : 상위 path 지정

--- src/main/java/com/private_lbs/taskmaster/favorite/controller/FavoriteController.java
+++ src/main/java/com/private_lbs/taskmaster/favorite/controller/FavoriteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("/favorite")
 @RequiredArgsConstructor
 public class FavoriteController {
 
