66af131662a234785c5aad49c826680acec1cdc6
hamseungchan <head0618@naver.com>
2024-02-05 14:54:23 +0900

    feat : 선수 service 구현

---
+++ src/main/java/com/private_lbs/taskmaster/player/domain/service/PlayerService.java
@@ -0,0 +1,20 @@
+package com.private_lbs.taskmaster.player.domain.service;
+
+import com.private_lbs.taskmaster.player.domain.Player;
+import com.private_lbs.taskmaster.player.domain.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    public List<Player> getPlayerFromRequest(long request) {
+        return playerRepository.findPlayer(request);
+    }
+
+}
