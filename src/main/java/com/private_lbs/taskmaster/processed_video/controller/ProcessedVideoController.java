ae25f130dc8b055f6a1f856a6162ad1ea836ee5d
hamseungchan <head0618@naver.com>
2024-02-05 14:41:28 +0900

    feat : 가공된 영상 컨트롤러 구현

---
+++ src/main/java/com/private_lbs/taskmaster/processed_video/controller/ProcessedVideoController.java
@@ -0,0 +1,27 @@
+package com.private_lbs.taskmaster.processed_video.controller;
+
+import com.private_lbs.taskmaster.global.auth.Auth;
+import com.private_lbs.taskmaster.processed_video.data.dto.response.ProcessedVideoFromPlayer;
+import com.private_lbs.taskmaster.processed_video.service.ProcessedVideoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/processed-video")
+@RequiredArgsConstructor
+public class ProcessedVideoController {
+
+    private final ProcessedVideoService processedVideoService;
+
+    @Auth
+    @GetMapping("/{request}")
+    public ResponseEntity<List<ProcessedVideoFromPlayer>> getProcessedVideos(@PathVariable Long request) {
+        return ResponseEntity.ok().body(processedVideoService.getPlayerProcessedVideo(request));
+    }
+}
