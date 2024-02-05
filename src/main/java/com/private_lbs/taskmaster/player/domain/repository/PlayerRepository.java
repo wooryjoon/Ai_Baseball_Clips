0c9a4546c5cf03a3a2e33b52f61d799becd3032b
hamseungchan <head0618@naver.com>
2024-02-05 14:54:34 +0900

    feat : 선수 repo 구현

--- src/main/java/com/private_lbs/taskmaster/player/domain/repository/PlayerRepository.java
+++ src/main/java/com/private_lbs/taskmaster/player/domain/repository/PlayerRepository.java
@@ -1,7 +1,13 @@
 package com.private_lbs.taskmaster.player.domain.repository;
 
 import com.private_lbs.taskmaster.player.domain.Player;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    @Query("select p from Player p where p.request.id = :requestId")
+    List<Player> findPlayer(@Param("requestId") long requestId);
 }
