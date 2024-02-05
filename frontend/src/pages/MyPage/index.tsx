4f3ec8ff7de3c7e915ab1aa61f28f44ab9dbb965
박희준 <wooryjoon@gmail.com>
2024-02-05 17:24:37 +0900

    Feat : 마이페이지 찜 목록 구현

--- frontend/src/pages/MyPage/index.tsx
+++ frontend/src/pages/MyPage/index.tsx
@@ -2,8 +2,13 @@ import Header from '@/components/Header';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './MyPage.scss';
+import MyContent from './MyContent';
+import Button from '@/components/Button';
 export default function MyPage() {
     const dummy = Array.from(Array(10).keys());
+    const onClickModifyHandler = () => {
+        console.log(123);
+    };
     return (
         <>
             <Header />
@@ -16,13 +21,19 @@ export default function MyPage() {
                     <span className="profile-name">힘숨찬</span>
                     <div className="profile-email">cypher123@naver.com</div>
                 </div>
+                <Button styleType="modify" onClick={onClickModifyHandler}>
+                    정보 수정
+                </Button>
             </div>
             {/* BookmarkList */}
             <div className="bookmarkList">
-                <div className="bookmark-title">내가 찜한 영상</div>
+                <div className="bookmark-title">
+                    내가 찜한 영상
+                    <img src="/src/assets/ball.png" alt="" />
+                </div>
                 <ul>
-                    {dummy.map((i) => (
-                        <li>{i}</li>
+                    {dummy.map(() => (
+                        <MyContent />
                     ))}
                 </ul>
             </div>
