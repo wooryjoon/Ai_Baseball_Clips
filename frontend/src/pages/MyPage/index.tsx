1eb3c72770fbf3cb16e087d04133a8a4d99b390f
박희준 <wooryjoon@gmail.com>
2024-02-05 15:33:54 +0900

    Feat : 마이페이지 UI 레이아웃 구현

--- frontend/src/pages/MyPage/index.tsx
+++ frontend/src/pages/MyPage/index.tsx
@@ -1,10 +1,31 @@
 import Header from '@/components/Header';
-
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './MyPage.scss';
 export default function MyPage() {
+    const dummy = Array.from(Array(10).keys());
     return (
         <>
             <Header />
-            <p> 마이 페이지다 </p>
+            {/* Profile Container */}
+            <div className="myProfile">
+                <div className="profile-img-container">
+                    <FontAwesomeIcon className="user" icon={faUser} />
+                </div>
+                <div className="profile-info-container">
+                    <span className="profile-name">힘숨찬</span>
+                    <div className="profile-email">cypher123@naver.com</div>
+                </div>
+            </div>
+            {/* BookmarkList */}
+            <div className="bookmarkList">
+                <div className="bookmark-title">내가 찜한 영상</div>
+                <ul>
+                    {dummy.map((i) => (
+                        <li>{i}</li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
 }
