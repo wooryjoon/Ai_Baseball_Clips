9a7f7bed0473a6081cb9921f3915883763b106f4
우창진 <deu03161@naver.com>
2024-02-06 17:15:07 +0900

    chore : Redis 설정 주입

--- ai/src/main/java/private_lbs/ai/RedisConfig.java
+++ ai/src/main/java/private_lbs/ai/RedisConfig.java
@@ -2,9 +2,11 @@ package private_lbs.ai;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -20,11 +22,25 @@ import private_lbs.ai.subscriber.RedisMessageSubscriber;
 public class RedisConfig {
 
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory();
-    }
+        RedisStandaloneConfiguration configuration=new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
 
+        return new LettuceConnectionFactory(configuration);
+    }
     @Bean
     public RedisTemplate<String,Object> redisTemplate(){
         RedisTemplate<String,Object> redisTemplate=new RedisTemplate<>();
