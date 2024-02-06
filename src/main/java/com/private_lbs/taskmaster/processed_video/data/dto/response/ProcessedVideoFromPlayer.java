6e95413b23e2f9875969125c240d064f0b84e820
hamseungchan <head0618@naver.com>
2024-02-06 13:14:54 +0900

    feat : jpql 에 dto 사용하기 위해 생성자 추가

--- src/main/java/com/private_lbs/taskmaster/processed_video/data/dto/response/ProcessedVideoFromPlayer.java
+++ src/main/java/com/private_lbs/taskmaster/processed_video/data/dto/response/ProcessedVideoFromPlayer.java
@@ -1,5 +1,6 @@
 package com.private_lbs.taskmaster.processed_video.data.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Builder
 @Getter
+@AllArgsConstructor
 public class ProcessedVideoFromPlayer {
     private String playerName;
     private List<ProcessedVideoResponse> processedVideoResponses;
