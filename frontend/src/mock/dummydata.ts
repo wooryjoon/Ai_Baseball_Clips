737f3323f7f5bc0df72709c4fa75c66e80bd0a76
박희준 <wooryjoon@gmail.com>
2024-02-08 01:10:55 +0900

    Feat : 레포트 페이지 UI 구현 70% 완료

--- frontend/src/mock/dummydata.ts
+++ frontend/src/mock/dummydata.ts
@@ -14,7 +14,8 @@ const clipData = Array.from(Array(1000).keys()).map(
         ),
     })
 );
-const pos = ['1루수', '2루수', '3루수', '중견수', '좌익수', '우익수', '유격수', '포수', '지명타자'];
+const pos = ['좌익수', '중견수', '우익수', '유격수', '2루수', '3루수', '1루수', '지명타자', '포수'];
+// const pos = ['1루수', '2루수', '3루수', '중견수', '좌익수', '우익수', '유격수', '포수', '지명타자'];
 const overviewPlayData: any = Array.from(Array(9).keys()).map((id) => {
     return {
         id,
--- frontend/src/pages/VideoResultPage/VideoResultOverview/VideoResultOverview.tsx
+++ frontend/src/pages/VideoResultPage/VideoResultOverview/VideoResultOverview.tsx
@@ -12,11 +12,9 @@ export default function VideoResultOverview() {
     //TODO stadium div의 width와 height를 계산해 stadium에 전달
     const stadiumContainerRef = useRef<HTMLDivElement>(null);
     const { width, height } = useCalculateWidthHeight(stadiumContainerRef);
-    const [canvasWidth, canvasHeight] = [width, height];
     const [currentTeam, setcurrentTeam] = useState<SelectedTeam>('team1');
 
     const onClickChangeTeam = (e: React.MouseEvent<HTMLDivElement>) => {
-        console.log(e.currentTarget.classList);
         if (e.currentTarget instanceof HTMLElement) {
             if (currentTeam === 'team1' && e.currentTarget.classList.contains('team1')) return;
             else if (currentTeam === 'team2' && e.currentTarget.classList.contains('team2')) return;
@@ -38,7 +36,7 @@ export default function VideoResultOverview() {
                           <PlayerCircle data={data} src="/src/assets/선수2.png" />
                       ))}
                 {/* 야구장 이미지 */}
-                <Stadium canvasWidth={canvasWidth} canvasHeight={canvasHeight} />
+                <Stadium canvasWidth={width} canvasHeight={height} />
             </div>
         </>
     );
