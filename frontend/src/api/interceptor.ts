39fbcc678a623d76f04dc14172465b4767682c48
박희준 <wooryjoon@gmail.com>
2024-02-08 15:03:16 +0900

    Feat : 리프레시 토큰 재발급 API 구현

--- frontend/src/api/interceptor.ts
+++ frontend/src/api/interceptor.ts
@@ -1,4 +1,5 @@
 import { Axios } from 'axios';
+import requestAccessToken from './requestAccessToken';
 
 export default function addAxiosInterceptor(instance: Axios) {
     instance.interceptors.request.use(
@@ -14,6 +15,26 @@ export default function addAxiosInterceptor(instance: Axios) {
         },
         (error) => {
             return Promise.reject(error);
+        }
+    );
+    // 403에러 가로챌 시 액세스 토큰 재발급 하고 원래 요청다시 하기!
+    instance.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            if (error.response && error.response.status === 403) {
+                const accessToken = sessionStorage.getItem('accessToken');
+                const refreshToken = sessionStorage.getItem('refreshToken');
+                if (accessToken && refreshToken) {
+                    requestAccessToken({ accessToken, refreshToken })
+                        .then((response) => {
+                            const newAccessToken = response.data.accessToken();
+                            sessionStorage.addItem('accessToken', newAccessToken);
+                            error.config.headers['Authorization'] = `${newAccessToken}`;
+                            return instance.request(error.config);
+                        })
+                        .catch(() => {});
+                }
+            }
         }
     );
 }
