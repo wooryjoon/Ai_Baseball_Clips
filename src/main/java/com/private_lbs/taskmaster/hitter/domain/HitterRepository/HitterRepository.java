441782370c8ba317ca281e7e82a95f1b9940201d
hamseungchan <head0618@naver.com>
2024-02-09 15:19:12 +0900

    feat : 새로운 테이블 구조에 맞게 entity와 repository 생성

---
+++ src/main/java/com/private_lbs/taskmaster/hitter/domain/HitterRepository/HitterRepository.java
@@ -0,0 +1,8 @@
+package com.private_lbs.taskmaster.hitter.domain.HitterRepository;
+
+import com.private_lbs.taskmaster.hitter.domain.Hitter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HitterRepository extends JpaRepository<Hitter,Long> {
+
+}
