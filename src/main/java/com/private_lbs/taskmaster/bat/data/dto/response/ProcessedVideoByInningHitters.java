6ed055bbd5dbe453962121546130eeea735e00c2
hamseungchan <head0618@naver.com>
2024-02-10 21:49:48 +0900

    refactor : lombok 으로 대체

--- src/main/java/com/private_lbs/taskmaster/bat/data/dto/response/ProcessedVideoByInningHitters.java
+++ src/main/java/com/private_lbs/taskmaster/bat/data/dto/response/ProcessedVideoByInningHitters.java
@@ -1,20 +1,14 @@
 package com.private_lbs.taskmaster.bat.data.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProcessedVideoByInningHitters {
     private String processedVideo;
     private Long hitterId;
     private String pitcherName;
-    private Boolean isFavorite;
-
-    public ProcessedVideoByInningHitters(String processedVideo, Long hitterId, String pitcherName, Boolean isFavorite) {
-        this.processedVideo = processedVideo;
-        this.hitterId = hitterId;
-        this.pitcherName = pitcherName;
-        this.isFavorite = isFavorite;
-    }
-
+    private Boolean Favorite;
 }
 
