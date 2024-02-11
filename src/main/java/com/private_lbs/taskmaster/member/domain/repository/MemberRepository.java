b50e5af95369b7666c3212373abe4780ce7401ef
hamseungchan <head0618@naver.com>
2024-02-11 19:20:32 +0900

    style : 사용하지 않는 import 제거

--- src/main/java/com/private_lbs/taskmaster/member/domain/repository/MemberRepository.java
+++ src/main/java/com/private_lbs/taskmaster/member/domain/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package com.private_lbs.taskmaster.member.domain.repository;
 
 import com.private_lbs.taskmaster.member.domain.Member;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
