e5b187096945174604317baad0c5804ce59c0881
hamseungchan <head0618@naver.com>
2024-02-12 14:10:29 +0900

    refactor : 로직 변경으로 사용하지 않는 메소드 제거

--- src/main/java/com/private_lbs/taskmaster/bat/service/BatService.java
+++ src/main/java/com/private_lbs/taskmaster/bat/service/BatService.java
@@ -16,51 +16,6 @@ public class BatService {
 
     private final BatQueryRepository batQueryRepository;
 
-    public Map<String, List<ProcessedVideoByHitter>> getProcessedVideoByHitters(long requestId) {
-        return batQueryRepository.findProcessedVideosByHitter(requestId).stream()
-                .collect(Collectors
-                        .groupingBy(ProcessedVideoByHitter::getHitterName));
-    }
-
-    public List<LineUpWithInningProcessedVideo> getProcessedVideoByInning(long requestId, int inning) {
-
-        // 경기에 출전한 선수의 리스트
-        List<Hitter> hitters = getTeamLineUp(requestId, inning);
-        // 선택한 이닝에 들어선 타석의 정보 hitterId를 key 로 설정해 Map 구조로 변경
-        Map<Long, ProcessedVideoByInningHitters> groupByHitterId =
-                batQueryRepository
-                        .getProcessedVideoByTeamAndInning(requestId, inning)
-                        .stream()
-                        .collect(
-                                Collectors.toMap(
-                                        ProcessedVideoByInningHitters::getHitterId,
-                                        Function.identity()
-                                )
-                        );
-
-        List<LineUpWithInningProcessedVideo> lineUpWithInningProcessedVideos = new ArrayList<>();
-        for (Hitter hitter : hitters) {
-            LineUpWithInningProcessedVideo lineUpWithInningProcessedVideo
-                    = new LineUpWithInningProcessedVideo(hitter.getName(), hitter.getPosition());
-
-            ProcessedVideoByInningHitters processedVideoByInningHitters = groupByHitterId.get(hitter.getId());
-            if (processedVideoByInningHitters != null) {
-                lineUpWithInningProcessedVideo.enrollProcessedVideo(processedVideoByInningHitters);
-            }
-            lineUpWithInningProcessedVideos.add(lineUpWithInningProcessedVideo);
-        }
-        return lineUpWithInningProcessedVideos;
-    }
-
-
-    private List<Hitter> getTeamLineUp(long requestId, int inning) {
-        return batQueryRepository.getHitters(requestId, inning % 2).stream()
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toCollection(LinkedHashSet::new),
-                        ArrayList::new
-                ));
-    }
-
     // 경기에 팀 정보 가져오기
     public TeamInfo getTeamInfo(long requestId) {
 
