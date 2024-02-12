a3caba179f61992f6164754d908772ebb6aae303
우창진 <deu03161@naver.com>
2024-02-12 17:42:54 +0900

    s3에 파일 업로드 시 SSE를 통한 업로드 완료 기능

--- src/main/java/com/private_lbs/taskmaster/redis/service/RedisSubService.java
+++ src/main/java/com/private_lbs/taskmaster/redis/service/RedisSubService.java
@@ -1,7 +1,6 @@
 package com.private_lbs.taskmaster.redis.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.private_lbs.taskmaster.redis.domain.RedisSubData;
 import com.private_lbs.taskmaster.redis.exception.RedisErrorCode;
 import com.private_lbs.taskmaster.redis.exception.RedisException;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +9,12 @@ import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class RedisSubService implements MessageListener {
     private final ObjectMapper mapper = new ObjectMapper();
-
+    private final SseEmitters sseEmitters;
     /**
      * @author boyjo
      * @date 2/3/24
@@ -25,9 +25,11 @@ public class RedisSubService implements MessageListener {
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
-            RedisSubData redisSubData = mapper.readValue(message.getBody(), RedisSubData.class);
             //TODO: 1. AI 가공이 완료되었다는 메시지가 수신 로직 구현 - SSE 필요
             //TODO: 2. 각 상태별 메시지가 수신 로직 구현 - SSE 필요
+            String mes=new String(message.getBody(),"UTF-8");
+//            sseEmitters.addEmitter();
+            sseEmitters.sendMessage(mes);
         } catch (RedisException | IOException e) {
             throw new RedisException(RedisErrorCode.MESSAGE_RECEIVE_FAILED);
         }
