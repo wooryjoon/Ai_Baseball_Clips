bcbb29cb6e6787fdfe873f10c42f72125a1f39a8
BoyCho <iijokv@naver.com>
2024-02-13 11:03:10 +0900

    feat : 선수 이미지url 추가

--- src/main/java/com/private_lbs/taskmaster/bat/controller/BatController.java
+++ src/main/java/com/private_lbs/taskmaster/bat/controller/BatController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/com/private_lbs/taskmaster/bat/data/dto/response/HitterNameAndImage.java
+++ src/main/java/com/private_lbs/taskmaster/bat/data/dto/response/HitterNameAndImage.java
@@ -2,28 +2,16 @@ package com.private_lbs.taskmaster.bat.data.dto.response;
 
 import lombok.Getter;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
-
 @Getter
 public class HitterNameAndImage {
 
     int inning;
     String name;
-    String imageBase64;
+    String imageUrl;
 
-    public HitterNameAndImage(int inning, String name, String imageUrl) throws IOException {
+    public HitterNameAndImage(int inning, String name, String imageUrl) {
         this.inning = inning;
         this.name = name;
-        this.imageBase64 = getImageBase64(imageUrl);
-    }
-
-    private String getImageBase64(String imageUrl) throws IOException {
-        Path path = Paths.get(imageUrl);
-        byte[] imageBytes = Files.readAllBytes(path);
-        return Base64.getEncoder().encodeToString(imageBytes);
+        this.imageUrl = imageUrl;
     }
 }
