bb6c3079be5ecf17bc0a20e2fb9b0a0c3dda9d32
박희준 <wooryjoon@gmail.com>
2024-02-13 17:33:54 +0900

    Feat : requestId 리덕스 저장 및 호출 시 사용 구현

--- frontend/src/api/requestReportView.ts
+++ frontend/src/api/requestReportView.ts
@@ -2,24 +2,36 @@ import { useQuery } from '@tanstack/react-query';
 import { instance } from '.';
 import { TeamInfo, TeamLineUp, TeamTimeLine } from './type';
 
-const requestTeamInfo = async () => {
-    return await instance.get<TeamInfo>(import.meta.env.VITE_API_BASE_URL + '/id/bat/team');
+const requestTeamInfo = async (reqId: number) => {
+    console.log(reqId);
+    return await instance.get<TeamInfo>(import.meta.env.VITE_API_BASE_URL + `/${reqId}/bat/team`);
 };
 
-const requestStartLineUp = async () => {
-    return await instance.get<TeamLineUp>(import.meta.env.VITE_API_BASE_URL + '/id/bat/line-up');
-};
-
-const requestTimeLine = async () => {
-    return await instance.get<TeamTimeLine>(
-        import.meta.env.VITE_API_BASE_URL + '/id/bat/time-line'
+const requestStartLineUp = async (reqId: number) => {
+    return await instance.get<TeamLineUp>(
+        import.meta.env.VITE_API_BASE_URL + `/${reqId}/bat/line-up`
     );
 };
 
-const reportPageQuery = () => {
-    const teamInfo = useQuery({ queryFn: requestTeamInfo, queryKey: ['parallel-teamInfo'] });
-    const lineUp = useQuery({ queryFn: requestStartLineUp, queryKey: ['parallel-startLineUp'] });
-    const timeLine = useQuery({ queryFn: requestTimeLine, queryKey: ['parallel-timeLine'] });
+const requestTimeLine = async (reqId: number) => {
+    return await instance.get<TeamTimeLine>(
+        import.meta.env.VITE_API_BASE_URL + `/${reqId}/bat/time-line`
+    );
+};
+
+const reportPageQuery = (reqId: number) => {
+    const teamInfo = useQuery({
+        queryFn: () => requestTeamInfo(reqId),
+        queryKey: ['parallel-teamInfo', reqId],
+    });
+    const lineUp = useQuery({
+        queryFn: () => requestStartLineUp(reqId),
+        queryKey: ['parallel-startLineUp', reqId],
+    });
+    const timeLine = useQuery({
+        queryFn: () => requestTimeLine(reqId),
+        queryKey: ['parallel-timeLine', reqId],
+    });
 
     const isLoading = teamInfo.isLoading && lineUp.isLoading && timeLine.isLoading;
     const isError = teamInfo.isError && lineUp.isError && timeLine.isError;
