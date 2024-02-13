cc87bcb696ff17f94a1786ac73355e7bf1ea0c12
hamseungchan <head0618@naver.com>
2024-02-13 13:34:57 +0900

    refactor : 찜 영상 가져오기

--- src/main/java/com/private_lbs/taskmaster/favorite/domain/repository/FavoriteQueryRepository.java
+++ src/main/java/com/private_lbs/taskmaster/favorite/domain/repository/FavoriteQueryRepository.java
@@ -1,6 +1,6 @@
 package com.private_lbs.taskmaster.favorite.domain.repository;
 
-import com.private_lbs.taskmaster.favorite.data.dto.response.FavoriteProcessedVideo;
+import com.private_lbs.taskmaster.bat.domain.Bat;
 import com.private_lbs.taskmaster.favorite.domain.Favorite;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +9,16 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface FavoriteQueryRepository extends JpaRepository<Favorite, Long> {
-    @Query("SELECT new com.private_lbs.taskmaster.favorite.data.dto.response." +
-            "FavoriteProcessedVideo(b.processedVideo,b.hitter.name, b.pitcher.name,f.isFavorite) " +
-            "FROM Favorite f " +
-            "INNER JOIN f.bat b " +
-            "INNER JOIN b.hitter h " +
-            "WHERE f.member.id = :memberId")
-    List<FavoriteProcessedVideo> findVideoLinkAndPlayerNameAndFavoriteByUserId(@Param("memberId") Long memberId);
+
+    @Query("SELECT DISTINCT b " +
+            "FROM Bat b " +
+            "JOIN FETCH b.hitter h " +
+            "JOIN FETCH b.pitcher p " +
+            "JOIN FETCH h.team ht " +
+            "JOIN FETCH p.team pt " +
+            "JOIN FETCH b.favorite f " +
+            "WHERE f.member.id = :memberId " +
+            "ORDER BY b.createDateTime")
+    List<Bat> getFavoriteProcessedVideo(@Param("memberId") long memberId);
+
 }
