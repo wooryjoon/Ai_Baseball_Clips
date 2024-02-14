aeaa7208677a56aa9698179093a5dd586b1e5709
minjeong <minj_1203@naver.com>
2024-02-14 10:38:27 +0900

    Fix: SSE 에러 수정

--- frontend/src/pages/VideoMakingPage/LoadingAI.tsx
+++ frontend/src/pages/VideoMakingPage/LoadingAI.tsx
@@ -1,6 +1,4 @@
 import Header from '@/components/Header';
-import { useEffect } from 'react';
-import { eventSource } from '@/api/sse';
 import ProgressBar from './ProgressBar';
 
 export default function LoadingAI() {
