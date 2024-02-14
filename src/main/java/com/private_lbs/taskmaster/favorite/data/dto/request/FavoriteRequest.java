b6aa48683160718738e610a688ae5493b4fd8be4
hamseungchan <head0618@naver.com>
2024-02-14 17:44:24 +0900

    bug : 기본 생성자 추가

--- src/main/java/com/private_lbs/taskmaster/favorite/data/dto/request/FavoriteRequest.java
+++ src/main/java/com/private_lbs/taskmaster/favorite/data/dto/request/FavoriteRequest.java
@@ -1,11 +1,14 @@
 package com.private_lbs.taskmaster.favorite.data.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FavoriteRequest {
 
     @NotNull
